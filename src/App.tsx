e53aa56a484b9f470f3aada3761516803728dfb6
Sebas <sajmansilla@gmail.com>
2024-11-25 10:36:38 +0100

    Update routes and add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-// App.tsx
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { LandingPage } from '@/components/landingpage';
 import { SurveyDashboard } from '@/components/surveys';
+import AddPeople from '@/components/AddPeople';
+import AddTeams from '@/components/AddTeams';
 import CreateSurvey from '@/components/CreateSurvey';
 import Report from '@/components/Report';
-import EditSurvey from '@/components/EditSurvey';
+import EditSurvey from '@/components/EditSurvey-bk';
+import TeamGaiaSurvey from '@/components/app_survey';
+import Header from '@/components/Header';
 import '@/App.css';
+
+// Componente para condicionalmente renderizar el Header
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  const isLandingPage = location.pathname === '/'; // Oculta el header solo en "/"
+
+  return (
+    <>
+      {!isLandingPage && <Header />}
+      <div className={!isLandingPage ? 'pt-20' : ''}>
+        {children}
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/surveys" element={<SurveyDashboard />} />
-        <Route path="/create-survey" element={<CreateSurvey />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="/edit-survey/:id" element={<EditSurvey />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/surveys" element={<SurveyDashboard />} />
+          <Route path="/people" element={<AddPeople />} />
+          <Route path="/teams" element={<AddTeams />} />
+          <Route path="/create-survey" element={<CreateSurvey />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/edit-survey/:survey_id" element={<EditSurvey />} />
+          <Route path="/team-gaia-survey" element={<TeamGaiaSurvey />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
