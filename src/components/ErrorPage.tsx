c1f6cbf36599eab38d28fa671b8838797c7df530
Sebas <sajmansilla@gmail.com>
2024-12-23 10:27:42 +0100

    Add ErrorPage component

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>The link is invalid or expired. Please contact support for help.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
